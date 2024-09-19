197a90a72b85639660639e907c252b322fe3b5e3
cava <104568530+caava@users.noreply.github.com>
2024-09-19 11:32:25 -0300

    Update Quermesse.py

--- OBI/Quermesse.py
+++ OBI/Quermesse.py
@@ -1,1 +1,16 @@
-https://humble-zebra-4p4q6jv45gg27jj5.github.dev/
+N = 1
+contador = 1
+
+lista = []
+testes = []
+
+while N != 0:
+    N = int(input("Número de participantes da festa: ")) # Declaração da variável número de participantes
+    ingresso = input("Informe a ordem de entrada: ")
+    lista = ingresso.split()
+    if contador == N:
+        for i in range(lista):
+            if (i+1) == lista[i]:
+                testes.append([f'Teste [{contador}]', {i + 1 }])
+
+    contador+=1
